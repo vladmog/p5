e8331e99883d3661a71424043a654411070444f7
Vladislav Mogilevskiy <vmogilevskiy@Vladislavs-MacBook-Pro.local>
2020-10-05 00:37:12 -0700

    pulled master

--- src/App.js
+++ src/App.js
@@ -24,7 +24,7 @@ if (width > cap) displayWidth = cap
         <h2>Day 1</h2>
         <h3>First animation</h3>
         <a href = "https://github.com/vladmog/p5/blob/master/src/components/day1.js">Code</a>
-        <Sketch1 canvasWidth = {displayWidth} canvasHeight = {displayWidth} />
+        <Sketch1 />
         
         <h2>Day 2</h2>
         <h3>
@@ -33,9 +33,9 @@ if (width > cap) displayWidth = cap
         <span>Generates unique image on each loading/reloading of page</span>
         <a href = "https://www.google.com/search?q=Structure+de+Quadrilateres&rlz=1C5CHFA_enUS906US906&sxsrf=ALeKk03OJbHl0234HwdPYZtSDsuHa7MmtA:1601866577357&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiMmMiCupzsAhXjGjQIHTHDAlsQ_AUoAXoECAsQAw&biw=1422&bih=801#imgrc=996Xl7KGglNRnM">Inspo</a>
         <a href = "https://github.com/vladmog/p5/blob/master/src/components/day2.js">Code</a>
-        <Sketch2 canvasWidth = {displayWidth} canvasHeight = {displayWidth} />
-        <Sketch4 canvasWidth = {displayWidth} canvasHeight = {displayWidth} />
-        <Sketch3 canvasWidth = {displayWidth} canvasHeight = {displayWidth} />
+        <Sketch2 />
+        <Sketch4 />
+        <Sketch3 />
       </header>
     </div>
   );
